34931a2163e6285ed66fa80f6394d5c85234a642
Tim Tröndle <tim.troendle@usys.ethz.ch>
2019-01-06 19:37:18 +0100

    Failed attempt to add thick border on hover

--- style.js
+++ style.js
@@ -1,7 +1,8 @@
 const COLOR_LIKELY_POSSIBLE = 'hsla(89, 47%, 40%, 0.4)';
 const COLOR_MAYBE_POSSIBLE = 'hsla(89, 47%, 40%, 0.2)';
 const COLOR_LIKELY_IMPOSSIBLE = 'hsla(2, 78%, 35%, 0.2)';
-const COLOR_IMPOSSIBLE = 'hsla(2, 78%, 35%, 0.4)'
+const COLOR_IMPOSSIBLE = 'hsla(2, 78%, 35%, 0.4)';
+const COLOR_HOVER = 'white';
 
 const CONDITIONAL_COLORING = [
     "step",
@@ -13,7 +14,12 @@ const CONDITIONAL_COLORING = [
     COLOR_MAYBE_POSSIBLE,
     10,
     COLOR_LIKELY_POSSIBLE
-    ]
+]
+const CONDITIONAL_BORDER = ["case",
+    ["boolean", ["feature-state", "hover"], false],
+    2,
+    0.0
+]
 
 function styleMap(map) {
     var layers = map.getStyle().layers;
@@ -25,13 +31,27 @@ function styleMap(map) {
             break;
         }
     }
+    map.addSource("europe", {
+        "type": "vector",
+        "url": "mapbox://timtroendle.91r5pfch"
+    });
+    map.addSource("country", {
+        "type": "vector",
+        "url": "mapbox://timtroendle.b2se99fd"
+    });
+    map.addSource("region", {
+        "type": "vector",
+        "url": "mapbox://timtroendle.0dly3483"
+    });
+    map.addSource("municipality", {
+        "type": "vector",
+        "url": "mapbox://timtroendle.8lu7xodw"
+    });
+
     map.addLayer({
         "id": "europe",
         "type": "fill",
-        "source": {
-            type: 'vector',
-            url: 'mapbox://timtroendle.91r5pfch'
-        },
+        "source": 'europe',
         "source-layer": "merged-results-5c45qb",
         "maxzoom": 3.5,
         "layout": {},
@@ -40,12 +60,21 @@ function styleMap(map) {
         }
     }, firstSymbolId);
     map.addLayer({
+        "id": "europe-borders",
+        "type": "line",
+        "source": 'europe',
+        "source-layer": "merged-results-5c45qb",
+        "maxzoom": 3.5,
+        "layout": {},
+        "paint": {
+            "line-color": COLOR_HOVER,
+            "line-width": CONDITIONAL_BORDER
+        }
+    }, firstSymbolId);
+    map.addLayer({
         "id": "country",
         "type": "fill",
-        "source": {
-            type: 'vector',
-            url: 'mapbox://timtroendle.b2se99fd'
-        },
+        "source": 'country',
         "source-layer": "merged-results-33ke27",
         "minzoom": 3.5,
         "maxzoom": 6,
@@ -57,12 +86,22 @@ function styleMap(map) {
         }
     }, firstSymbolId);
     map.addLayer({
+        "id": "country-borders",
+        "type": "line",
+        "source": 'country',
+        "source-layer": "merged-results-33ke27",
+        "minzoom": 3.5,
+        "maxzoom": 6,
+        "layout": {},
+        "paint": {
+            "line-color": COLOR_HOVER,
+            "line-width": CONDITIONAL_BORDER
+        }
+    }, firstSymbolId);
+    map.addLayer({
         "id": "region",
         "type": "fill",
-        "source": {
-            type: 'vector',
-            url: 'mapbox://timtroendle.0dly3483'
-        },
+        "source": 'region',
         "source-layer": "merged-results-2jcgj1",
         "minzoom": 6,
         "maxzoom": 9,
@@ -81,12 +120,22 @@ function styleMap(map) {
         }
     }, firstSymbolId);
     map.addLayer({
+        "id": "region-borders",
+        "type": "line",
+        "source": 'region',
+        "source-layer": "merged-results-2jcgj1",
+        "minzoom": 6,
+        "maxzoom": 9,
+        "layout": {},
+        "paint": {
+            "line-color": COLOR_HOVER,
+            "line-width": CONDITIONAL_BORDER
+        }
+    }, firstSymbolId);
+    map.addLayer({
         "id": "municipality",
         "type": "fill",
-        "source": {
-            type: 'vector',
-            url: 'mapbox://timtroendle.8lu7xodw'
-        },
+        "source": 'municipality',
         "source-layer": "merged-results-4xkc2x",
         "minzoom": 9,
         "layout": {},
@@ -101,6 +150,18 @@ function styleMap(map) {
                 10802090.043295115,
                 "#679436"
             ]
+        }
+    }, firstSymbolId);
+    map.addLayer({
+        "id": "municipality-borders",
+        "type": "line",
+        "source": 'municipality',
+        "source-layer": "merged-results-4xkc2x",
+        "minzoom": 9,
+        "layout": {},
+        "paint": {
+            "line-color": COLOR_HOVER,
+            "line-width": CONDITIONAL_BORDER
         }
     }, firstSymbolId);
 
